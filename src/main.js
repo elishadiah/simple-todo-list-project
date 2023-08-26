19f202910b6b9176c636ffa1853227bd1a766d2a
DESKTOP-EJF6S7M\Elisha Diah <elishadiah326@gmail.com>
2023-08-26 18:42:01 +0900

    using VueX

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './index.css'
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store'
+import './index.css';
+Vue.config.productionTip = false;
+
 
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
